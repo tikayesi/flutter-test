f0142d3e6158d6e63678edf31527b301138611bc
tikayesikristiani <yesikristianitika@gmail.com>
2021-09-28 14:01:56 +0700

    reusable widget 2

--- flutter_application_1/lib/button_of_number.dart
+++ flutter_application_1/lib/button_of_number.dart
@@ -1,7 +1,17 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class ButtonOfNumber extends StatelessWidget {
-  const ButtonOfNumber({ Key? key }) : super(key: key);
+  //const ButtonOfNumber({ Key? key }) : super(key: key);
+
+  // symbol ? => optional
+  final String number;
+  final double? elevationSize;
+  const ButtonOfNumber({required this.number, this.elevationSize});
+  // {} => bisa melakukan set sebuah attribute optional atau required
+  // {} => harus define nama atributenya ketika memanggil kelas ini
+  // {} => dapat memanggil attribute tidak berurutan
 
   @override
   Widget build(BuildContext context) {
@@ -9,11 +19,12 @@ class ButtonOfNumber extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: MaterialButton(
         onPressed: () {},
-        child: Text('1',
+        child: Text(number,
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)),
         color: Colors.amber[100],
         height: 75.0,
-        elevation: 20,
+        elevation: elevationSize ?? 10,
+        // elevation: elevationSize != null ? elevationSize : 10
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(18.0),
           side: BorderSide(color: Colors.blue),
--- flutter_application_1/lib/my_home_page.dart
+++ flutter_application_1/lib/my_home_page.dart
@@ -13,22 +13,29 @@ class MyHomePage extends StatefulWidget {
 // shift + alt + f -> win
 // shift + option + f -> mac
 // ctrl + shift + I -> linux
-class _MyHomePageState extends State<MyHomePage> {
 
-  MaterialButton _button(String number) {
-    return MaterialButton(
-      onPressed: () {},
-      child: Text(number,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)),
-      color: Colors.amber[100],
-      height: 75.0,
-      elevation: 20,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(18.0),
-        side: BorderSide(color: Colors.blue),
-      ),
-    );
-  }
+Column keyPadFun(){
+  List col = [
+    ['1', '2', '3'],
+    [ '4', '5', '6']
+    ];
+  return Column(
+    children: 
+    List.generate(col.length, (index) =>
+    _numberRow(col[index])
+    ));
+}
+
+Widget _numberRow(List<String> numbers){
+  return Row(children: List.generate(numbers.length, (index) =>
+  _buttonOfNumberBuilder(numbers[index])),);
+}
+
+Widget _buttonOfNumberBuilder(String num){
+  return ButtonOfNumber(number: num,);
+}
+
+class _MyHomePageState extends State<MyHomePage> {
 
 
   @override
@@ -58,21 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Column(
             children: [
-              // TextButton(
-              //     onPressed: () {},
-              //     child: Text('1'),
-              //     style: TextButton.styleFrom(primary: Colors.green)),
-              // ElevatedButton(
-              //   onPressed: () {},
-              //   child: Text('button'),
-              //   style: ElevatedButton.styleFrom(
-              //       primary: Colors.orange,
-              //       onPrimary: Colors.white),
-              // ),
-             _button('1'),
-             _button('2'),
-             _button('3'),
-             ButtonOfNumber()
+             keyPadFun()
             ],
           ),
         ],
